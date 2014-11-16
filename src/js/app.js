11b942f2853b9845ad6e6213905e0891759f1820
wvwvvvjs <wvwvvvjs@gmail.com>
2014-11-15 23:48:53 -0500

    Add basic routing

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,14 @@
 angular.module('wvwvvvjsApp', ['ngRoute'])
 
-.config(['$routeProvider',
-  function($routeProvider) {
+.config(['$routeProvider', '$locationProvider',
+  function($routeProvider, $locationProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'src/views/home.html'
+      })
+      .when('/1', {
+        templateUrl: 'src/views/viz/1.html'
       });
+
+    $locationProvider.html5Mode(true);
   }]);
